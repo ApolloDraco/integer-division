8f6d7d6719bcaddba75de6295271d039340bc7f9
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-11 17:21:16 -0400

    Update IntegerDivision.java

--- IntegerDivision.java
+++ IntegerDivision.java
@@ -3,23 +3,20 @@
 */
 
 import java.util.Scanner;
- 
-  public class IntegerDivision {
-  public static void main(String[] args) {
-  // Create a Scanner
-  Scanner input = new Scanner(System.in);
- 
-  // Receive an input
-  System.out.print("Enter an integer: ");
- int number = input.nextInt();
 
- if (number % 2 == 0 && number % 3 == 0)
- System.out.println(number + " is divisible by 2 and 3.");
- if (number % 2 == 0 || number % 3 == 0)
- System.out.println(number + " is divisible by 2 or 3.");
+public class IntegerDivision {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
- if (number % 2 == 0 ^ number % 3 == 0)
- System.out.println(number +
- " is divisible by 2 or 3, but not both.");
- }
- }
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0 && number % 3 == 0) {
+            System.out.println(number + " is divisible by both 2 and 3.");
+        } else if (number % 2 == 0 || number % 3 == 0) {
+            System.out.println(number + " is divisible by either 2 or 3.");
+        } else {
+            System.out.println(number + " is not divisible by either 2 or 3.");
+        }
+    }
+}
